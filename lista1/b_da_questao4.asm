635e49bfcf4f733be19e1a30a61da34515e326d7
Álisson Rodrigues Fernandes <97190682+alissonrodrigues0120@users.noreply.github.com>
2023-04-08 06:13:46 -0300

    Create b_da_questao4.asm

---
+++ lista1/b_da_questao4.asm
@@ -0,0 +1,21 @@
+;B da questão 4
+
+includelib msvcrt.lib
+includelib legacy_stdio_definitions.lib
+
+.686
+.model flat, c
+.stack 100h
+.data
+ y sdword ?
+ x sdword ?
+
+ .code
+
+ main proc
+      mov eax, y
+      mov x, eax
+      ret
+main endp
+     end
+
