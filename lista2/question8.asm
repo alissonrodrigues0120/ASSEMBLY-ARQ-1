97a9f75eb1a2f6fc1ca34898159d0ab90547c36b
Álisson Rodrigues Fernandes <97190682+alissonrodrigues0120@users.noreply.github.com>
2023-04-15 12:28:00 -0300

    Update question8.asm

--- lista2/question8.asm
+++ lista2/question8.asm
@@ -1,3 +1,10 @@
+;
+;   Building Instructions:
+;   ml /Cx /coff question8.asm /link /SUBSYSTEM:console /out:question8.exe kernel32.lib legacy_stdio_definitions.lib msvcrt.lib
+;
+
+
+
 .686
 .model flat, c
 .stack 100h
