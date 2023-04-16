89f9937903c9292da5e72f7d608b8d6f312f21e9
Álisson Rodrigues Fernandes <97190682+alissonrodrigues0120@users.noreply.github.com>
2023-04-15 22:41:15 -0300

    Update QUESTION6.asm

--- lista3/QUESTION6.asm
+++ lista3/QUESTION6.asm
@@ -1,3 +1,10 @@
+
+;
+;   Building Instructions:
+;   ml /Cx /coff QUESTION6.asm /link /SUBSYSTEM:console /out:QUESTION6.exe kernel32.lib legacy_stdio_definitions.lib msvcrt.lib
+;
+
+
 .686
 .model flat, c
 .stack 100h
