a6616012e4c76024bb9a839a643d9a9c42ba29f2
Álisson Rodrigues Fernandes <97190682+alissonrodrigues0120@users.noreply.github.com>
2023-06-01 20:31:18 -0300

    Create Questao_9_a.asm

---
+++ lista5/Questao_9_a.asm
@@ -0,0 +1,29 @@
+.686
+.model flat, c
+.stack 100h
+
+scanf PROTO arg2:Ptr Byte, inputlist:VARARG
+printf PROTO arg1:Ptr Byte, printlist:VARARG
+
+.data
+n sdword ?
+msgfmt byte "%s", 0Ah, 0
+msg1 byte "valor de n: ", 0
+msgfmt2 byte "%s%d", 0Ah, 0
+msginput byte "%d", 0
+
+.code
+main proc
+     INVOKE printf, ADDR msgfmt, ADDR msg1
+     INVOKE scanf, ADDR msginput, ADDR n
+     mov eax, 1
+     .while (n!=0)
+     imul n
+     dec n
+     .endw
+     mov n, eax
+
+     INVOKE printf, ADDR msgfmt2, ADDR msg1, n
+     ret
+main endp
+     end
