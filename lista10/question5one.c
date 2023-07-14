36ddee4c50b97db7e00a12cb832a987daad90d05
Álisson Rodrigues Fernandes <97190682+alissonrodrigues0120@users.noreply.github.com>
2023-07-13 23:14:46 -0300

    Create question5one.c

---
+++ lista10/question5one.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+  
+float number1, number2;
+printf("\n%s", "Enter a float of number1: ");
+scanf_s("%f", &number1);
+__asm {
+  
+    fld number1
+    fstp number2
+  
+   }
+printf("\n%s%6.4f\n\n", "The float of number2 is: ", number2);
+return 0;
+
+}
