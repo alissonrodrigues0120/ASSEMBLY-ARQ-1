2debe8ca82e3c642d44642301a6fc3a14edd6a3a
Álisson Rodrigues Fernandes <97190682+alissonrodrigues0120@users.noreply.github.com>
2023-07-13 23:43:21 -0300

    Update question6b.asm

--- lista8/question6b.asm
+++ lista8/question6b.asm
@@ -1,12 +1,12 @@
 ;compile:
 ;   ml /Cx /coff SelectionSort.asm /link /SUBSYSTEM:console  /out:SelectionSort.exe kernel32.lib legacy_stdio_definitions.lib msvcrt.lib
 ;   
-          .686
-          .model  flat, c
-          .stack  100h
+.686
+.model  flat, c
+.stack  100h
 printf    PROTO   arg1:Ptr Byte, printlist:VARARG
 scanf     PROTO   arg2:Ptr Byte, inputlist:VARARG
-          .data
+.data
 msg1fmt   byte    0Ah, "%s", 0
 msg2fmt   byte    "%s", 0
 msg4fmt   byte    "   %d", 0Ah, 0
@@ -18,7 +18,7 @@ n         sdword  ?
 array     sdword  20 dup(?)
 temp      sdword  ?
           .code
-main      proc
+main   proc
           INVOKE printf, ADDR msg1fmt, ADDR msg1
           INVOKE scanf, ADDR inputfmt, ADDR n
           INVOKE printf, ADDR pular
