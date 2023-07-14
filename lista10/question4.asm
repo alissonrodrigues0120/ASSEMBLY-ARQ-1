fa24fa99c1630e2c484d930aef9626d288064732
Álisson Rodrigues Fernandes <97190682+alissonrodrigues0120@users.noreply.github.com>
2023-07-13 23:03:45 -0300

    Create question4.asm

---
+++ lista10/question4.asm
@@ -0,0 +1,25 @@
+;Compile:
+ ;  ml /Cx /coff question4.asm /link /SUBSYSTEM:console /out:question4.exe kernel32.lib legacy_stdio_definitions.lib msvcrt.lib
+ ;
+.686
+.model flat, c
+.stack 100h
+scanf PROTO arg2:Ptr Byte, inputlist:VARARG
+printf PROTO arg1:Ptr Byte, printlist:VARARG
+.data
+inputfmt byte "%lf",0
+msg1fmt  byte 0Ah, "%s", 0
+msg1 byte "Enter a long double for x: ", 0
+msg2  byte 0Ah, "The long double is: ", "%6.4lf", 0Ah, 0
+x  real10 ?
+y  real10 ?
+
+.code
+main proc
+     INVOKE printf, ADDR msg1fmt, ADDR msg1
+     INVOKE scanf, ADDR inputfmt, ADDR x
+     fld x
+     fstp y
+     INVOKE printf, ADDR msg2, y
+main  endp
+      end
